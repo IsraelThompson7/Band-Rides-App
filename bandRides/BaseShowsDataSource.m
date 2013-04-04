e6f4854757090e9ec7bd02a46483b3379a50a07a
Hiram Hibbard <hiram@wearehathway.com>
2013-04-03 17:55:59 -0700

    Moving labels for landscape view

--- bandRides/BaseShowsDataSource.m
+++ bandRides/BaseShowsDataSource.m
@@ -43,29 +43,33 @@
         //Landscape only code
         [cell addSubview:[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"first.png"]]];//Added this image as a demo Hiram please replace with soemthing meaningful
         
-        UILabel *BandName = [[UILabel alloc] initWithFrame:CGRectMake(10, 5, 320, 20)];
+        UILabel *BandName = [[UILabel alloc] initWithFrame:CGRectMake(15, 10, 320, 20)];
         BandName.text = show.bandName;
         
-        UILabel *ShowDate = [[UILabel alloc] initWithFrame:CGRectMake(10, 25, 200, 20)];
-        ShowDate.text = show.Date;
-        [ShowDate setFont:[UIFont systemFontOfSize:12]];
-        
-        // To Do: Replace hard coded venue value with real data
-        UILabel *ShowVenue = [ [UILabel alloc] initWithFrame:CGRectMake(200, 25, 150, 20)];
+        UILabel *ShowVenue = [ [UILabel alloc] initWithFrame:CGRectMake(15, 30, 200, 20)];
         ShowVenue.text = show.venueName;
         [ShowVenue setFont:[UIFont systemFontOfSize:12]];
         
-        UILabel *LocationAddress = [[UILabel alloc] initWithFrame:CGRectMake(10, 50, 320, 20)];
+        UILabel *ShowDate = [[UILabel alloc] initWithFrame:CGRectMake(200, 30, 200, 20)];
+        ShowDate.text = show.Date;
+        [ShowDate setFont:[UIFont systemFontOfSize:12]];
+        
+        UILabel *LocationAddress = [[UILabel alloc] initWithFrame:CGRectMake(15, 50, 320, 20)];
         LocationAddress.text = show.Location_Address;
         [LocationAddress setFont:[UIFont systemFontOfSize:12]];
         
+        UILabel *LocationCity = [[UILabel alloc] initWithFrame:CGRectMake(15, 50, 320, 20)];
+        LocationCity.text = show.Location_Address;
+        [LocationCity setFont:[UIFont systemFontOfSize:12]];
+        
         [cell addSubview:BandName];
-        [cell addSubview:ShowDate];
         [cell addSubview:ShowVenue];
+        [cell addSubview:ShowDate];
         
         //If this is the row the user selected
         if (self.path.row == indexPath.row) {
             [cell addSubview:LocationAddress];
+            [cell addSubview:LocationCity];
         }
     } else {
         //Portrait only code
