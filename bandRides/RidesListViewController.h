8c2843f7fee392182ee54d146f35c26e176f88ad
Kevin Strong <kevin.strong@wearehathway.com>
2013-04-03 17:39:44 -0700

    fix sendSMS to use MFMessageComposeViewController

--- bandRides/RidesListViewController.h
+++ bandRides/RidesListViewController.h
@@ -9,8 +9,9 @@
 #import <UIKit/UIKit.h>
 #import "ShowData.h"
 #import <MessageUI/MFMailComposeViewController.h>
+#import <MessageUI/MFMessageComposeViewController.h>
 
-@interface RidesListViewController : UITableViewController <UIAlertViewDelegate,MFMailComposeViewControllerDelegate>
+@interface RidesListViewController : UITableViewController <UIAlertViewDelegate,MFMailComposeViewControllerDelegate, MFMessageComposeViewControllerDelegate>
 
 @property (nonatomic, strong) ShowData *show;
 @property (nonatomic, strong) NSMutableArray *ridesArray;
